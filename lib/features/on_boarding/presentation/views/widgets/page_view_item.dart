65da042160c3fed3b33f7dce7a694cb6ec0bb733
Mora <mohanad1481979@gmail.com>
2025-10-15 20:48:12 +0300

    page view item part 2 done

--- lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PageViewItem extends StatelessWidget {
-  const PageViewItem({super.key, required this.image, required this.backgroundImage, required this.subtitle, required this.title});
+  const PageViewItem(
+      {super.key,
+      required this.image,
+      required this.backgroundImage,
+      required this.subtitle,
+      required this.title});
 
   final String image, backgroundImage;
   final String subtitle;
@@ -9,6 +17,36 @@ class PageViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                    child: SvgPicture.asset(
+                  backgroundImage,
+                  fit: BoxFit.fill,
+                )),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: SvgPicture.asset(
+                    image,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text('تخط'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_app/core/helper_functions/on_generate_routes.dart';
 import 'package:fruit_app/features/splash/presentation/views/splash_view.dart';
+import 'package:fruit_app/generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('ar'),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
