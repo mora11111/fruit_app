30fb121499e68646d423d3238765aef437f62cb4
Mora <mohanad1481979@gmail.com>
2025-10-16 10:22:57 +0300

    adding page controller to listen to current page

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -6,8 +6,33 @@ import 'package:fruit_app/core/utils/app_color.dart';
 import 'package:fruit_app/core/widgets/custom_button.dart';
 import 'package:fruit_app/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController pageController;
+
+  var currentPage = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +46,16 @@ class OnBoardingViewBody extends StatelessWidget {
             color: AppColors.primaryColor.withOpacity(.5),
           ),
         ),
-        const SizedBox(height: 29,),
+        const SizedBox(
+          height: 29,
+        ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
           child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
         ),
-        const SizedBox(height: 43,),
+        const SizedBox(
+          height: 43,
+        ),
       ],
     );
   }
